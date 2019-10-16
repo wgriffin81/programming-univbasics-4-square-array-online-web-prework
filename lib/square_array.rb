3f8ab9be93b41f9affbc110ecd08881e40211c4d
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-16 06:40:51 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -5,9 +5,11 @@
 def square_array(array)
   counter = 0
   while counter < array.length do
+    new_array = []
+    
     element = array[counter]
     squared_element = (element**element)
-    new_array = []
+    
     
     new_array << squared_element
     
