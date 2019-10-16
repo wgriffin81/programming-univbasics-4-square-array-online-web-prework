83b60c635375dfd98a9c80020f80ae3b6ad4b364
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-16 06:34:59 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,15 @@
+# square each element in array 
+#return a new array of square numbers
+
+
 def square_array(array)
-  # your code here
+  counter = 0
+  while counter < array.length 
+    element = array[counter]
+    squared_element = element**element
+    
+    new_array.new << squared_element
+  end
+  
+  return new_array
 end
